378f37751933714ada93e7108361b5af5fa6523c
AkmalKhairi <akmalkhairi24@gmail.com>
2021-08-08 12:46:16 +0800

    Latest Update: 1. Conn.php (includes, voter, admin, superadmin) File Remote Connection
    Next Issue:
    1. Able to send message to voter who do not vote.

--- admin/includes/conn.php
+++ admin/includes/conn.php
@@ -1,9 +1,9 @@
 <?php
 //DEVELOPMENT CONNECTION
-/*$conn = new mysqli('localhost', 'root', '', 'votesystem');*/
+$conn = new mysqli('localhost', 'root', '', 'votesystem');
 
 //REMOTE CONNECTION
-$conn = new mysqli('remotemysql.com', 'OchgSe0SEu', 'AfYgpkIlWY', 'OchgSe0SEu');
+/*$conn = new mysqli('remotemysql.com', 'OchgSe0SEu', 'AfYgpkIlWY', 'OchgSe0SEu');*/
 
 	if ($conn->connect_error) {
 	    die("Connection failed: " . $conn->connect_error);
--- voter/includes/conn.php
+++ voter/includes/conn.php
@@ -1,9 +1,9 @@
 <?php
-    //DEVELOPMENT CONNECTION
-	/*$conn = new mysqli('localhost', 'root', '', 'votesystem');*/
+//DEVELOPMENT CONNECTION
+$conn = new mysqli('localhost', 'root', '', 'votesystem');
 
-    //REMOTE CONNECTION
-    $conn = new mysqli('remotemysql.com', 'OchgSe0SEu', 'AfYgpkIlWY', 'OchgSe0SEu');
+//REMOTE CONNECTION
+/*$conn = new mysqli('remotemysql.com', 'OchgSe0SEu', 'AfYgpkIlWY', 'OchgSe0SEu');*/
 
 	if ($conn->connect_error) {
 	    die("Connection failed: " . $conn->connect_error);
