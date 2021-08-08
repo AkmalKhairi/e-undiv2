e7c97ea451d497bfc2349158444dd74d6d45842c
AkmalKhairi <akmalkhairi24@gmail.com>
2021-08-08 12:22:44 +0800

    Latest Update: 1. Conn.php (includes, voter, admin, superadmin) File Remote Connection
    Next Issue:
    1. Able to send message to voter who do not vote.

--- voter/includes/conn.php
+++ voter/includes/conn.php
@@ -1,5 +1,9 @@
 <?php
-	$conn = new mysqli('localhost', 'root', '', 'votesystem');
+    //DEVELOPMENT CONNECTION
+	/*$conn = new mysqli('localhost', 'root', '', 'votesystem');*/
+
+    //REMOTE CONNECTION
+    $conn = new mysqli('remotemysql.com', 'OchgSe0SEu', 'AfYgpkIlWY', 'OchgSe0SEu');
 
 	if ($conn->connect_error) {
 	    die("Connection failed: " . $conn->connect_error);
