a64ea767a5462814a7b3b12641560593f6911af1
AkmalKhairi <akmalkhairi24@gmail.com>
2021-08-10 17:27:11 +0800

    Latest Update: 1. Google ReCAPTCHA
    Next Issue:
    1. Able to send message to voter who do not vote.

--- voter/voters_add.php
+++ voter/voters_add.php
@@ -1,7 +1,7 @@
 <?php
 include 'includes/session.php';
 
-if(isset($_POST['add'])){
+if(isset($_POST['add'])) {
     $firstname = $_POST['firstname'];
     $lastname = $_POST['lastname'];
     $password = password_hash($_POST['password'], PASSWORD_DEFAULT);
@@ -9,39 +9,36 @@ if(isset($_POST['add'])){
     $date = $_POST['date'];
     $phone = $_POST['phone'];
     $filename = $_FILES['photo']['name'];
-
-    $PRIVATE_KEY = '6LeLeuEbAAAAAIQscuFINT8u3GIGmXi7w2bnfdjE';
-    $responseKey = $_POST['g-recaptcha-response'];
-    $userIP = $_SERVER['REMOTE_ADDR'];
-
-    if(!empty($filename)){
-        move_uploaded_file($_FILES['photo']['tmp_name'], '../images/'.$filename);
+    if (!empty($filename)) {
+        move_uploaded_file($_FILES['photo']['tmp_name'], '../images/' . $filename);
     }
-
-    $url = "https://www.google.com/recaptcha/api/siteverify?secret=$PRIVATE_KEY&response=$responseKey&remoteip=$userIP";
-    $response = file_get_contents($url);
-    $response = json_decode($response);
-
-    $sql2 = mysqli_query($conn,"SELECT voters_id from voters WHERE voters_id = '$voter' AND status='Not Verified'");
-    if (mysqli_num_rows($sql2)==1) {
+    $sql2 = mysqli_query($conn, "SELECT voters_id from voters WHERE voters_id = '$voter' AND status='Not Verified'");
+    if (mysqli_num_rows($sql2) == 1) {
         $_SESSION['error'] = 'User already exist';
-    }
+    } // Validate reCAPTCHA box
+    elseif (isset($_POST['g-recaptcha-response']) && !empty($_POST['g-recaptcha-response'])) {
+        // Google reCAPTCHA API secret key
+        $secretKey = '6LeLeuEbAAAAAIQscuFINT8u3GIGmXi7w2bnfdjE';
 
-    elseif ($response->success)
-    {
-        $date = date('Y-m-d');
-        $status = "Not Verified";
-        $sql = "INSERT INTO voters (voters_id, password, firstname, lastname, created_on, photo, status, phone) VALUES ('$voter', '$password', '$firstname', '$lastname', '$date' , '$filename', '$status','$phone')";
-        if($conn->query($sql)){
-            $_SESSION['success'] = 'Registration success';
-        }
-        else{
-            $_SESSION['error'] = $conn->error;
-        }
+        // Verify the reCAPTCHA response
+        $verifyResponse = file_get_contents('https://www.google.com/recaptcha/api/siteverify?secret=' . $secretKey . '&response=' . $_POST['g-recaptcha-response']);
 
-    }
-    else{
-        $_SESSION['error'] = 'Robot verification failed, please try again.';
+        // Decode json data
+        $responseData = json_decode($verifyResponse);
+
+        // If reCAPTCHA response is valid
+        if ($responseData->success) {
+            $date = date('Y-m-d');
+            $status = "Not Verified";
+            $sql = "INSERT INTO voters (voters_id, password, firstname, lastname, created_on, photo, status, phone) VALUES ('$voter', '$password', '$firstname', '$lastname', '$date' , '$filename', '$status','$phone')";
+            if ($conn->query($sql)) {
+                $_SESSION['success'] = 'Registration success';
+            } else {
+                $_SESSION['error'] = $conn->error;
+            }
+        } else {
+            $_SESSION['error'] = 'Robot verification failed, please try again.';
+        }
     }
 }
 else{
