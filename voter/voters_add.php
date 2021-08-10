64288d85236c16f34592abd4aa9d51217497ef79
AkmalKhairi <akmalkhairi24@gmail.com>
2021-08-10 08:22:59 +0800

    Latest Update: 1. Conn.php (includes, voter, admin, superadmin) File Remote Connection
    Next Issue:
    1. Able to send message to voter who do not vote.

--- voter/voters_add.php
+++ voter/voters_add.php
@@ -9,23 +9,25 @@ if(isset($_POST['add'])){
     $date = $_POST['date'];
     $phone = $_POST['phone'];
     $filename = $_FILES['photo']['name'];
+
     $PRIVATE_KEY = '6LeLeuEbAAAAAIQscuFINT8u3GIGmXi7w2bnfdjE';
+    $responseKey = $_POST['g-recaptcha-response'];
+    $userIP = $_SERVER['REMOTE_ADDR'];
+
     if(!empty($filename)){
         move_uploaded_file($_FILES['photo']['tmp_name'], '../images/'.$filename);
     }
 
-    $url = "https://www.google.com/recaptcha/api/siteverify";
-
-    $remoteServer = filter_input(INPUT_SERVER, 'REMOTE_ADDR', FILTER_SANITIZE_URL);
-    $response = file_get_contents($url . "?secret=" . $PRIVATE_KEY . "&response=" . \htmlspecialchars($_POST['g-recaptcha-response']) . "&remoteip=" . $remoteServer);
-    $recaptcha_data = json_decode($response);
+    $url = "https://www.google.com/recaptcha/api/siteverify?secret=$PRIVATE_KEY&response=$responseKey&remoteip=$userIP";
+    $response = file_get_contents($url);
+    $response = json_decode($response);
 
     $sql2 = mysqli_query($conn,"SELECT voters_id from voters WHERE voters_id = '$voter' AND status='Not Verified'");
     if (mysqli_num_rows($sql2)==1) {
         $_SESSION['error'] = 'User already exist';
     }
 
-    elseif (isset($recaptcha_data->success) && $recaptcha_data->success === TRUE)
+    elseif ($response->success)
     {
         $date = date('Y-m-d');
         $status = "Not Verified";
