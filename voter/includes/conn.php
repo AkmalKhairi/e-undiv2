425ca6b5758caf09b0827d9eb073454df1061dc0
AkmalKhairi <akmalkhairi24@gmail.com>
2021-09-07 13:17:18 +0800

    Latest Update: 1. Fix Name input cannot have any special characters 2. Fix soft delete on Positions
    Next Issue:
    1.

--- voter/includes/conn.php
+++ voter/includes/conn.php
@@ -1,12 +1,12 @@
 <?php
 //DEVELOPMENT CONNECTION
-$conn = new mysqli('localhost', 'root', '', 'votesystem');
+/*$conn = new mysqli('localhost', 'root', '', 'votesystem');*/
 
 //REMOTE CONNECTION
 /*$conn = new mysqli('remotemysql.com', 'OchgSe0SEu', 'AfYgpkIlWY', 'OchgSe0SEu');*/
 
 //JAWSDB CONNECTION
-/*$conn = new mysqli('ro2padgkirvcf55m.cbetxkdyhwsb.us-east-1.rds.amazonaws.com', 'gzrevp3i7iopfcb5', 'z9ocealdamxnd3r2', 'b4dnswyw8qdfmmbd');*/
+$conn = new mysqli('ro2padgkirvcf55m.cbetxkdyhwsb.us-east-1.rds.amazonaws.com', 'gzrevp3i7iopfcb5', 'z9ocealdamxnd3r2', 'b4dnswyw8qdfmmbd');
 
 if ($conn->connect_error) {
     die("Connection failed: " . $conn->connect_error);
